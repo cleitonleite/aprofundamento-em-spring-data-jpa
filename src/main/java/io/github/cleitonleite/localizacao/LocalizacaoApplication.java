bba95d75da816888ef048bbe79f92ba66168b5e9
Cleiton Leite <cleitonleite.melo@gmail.com>
2024-02-01 23:31:18 -0300

    Testando o repositorio de Cidades

--- src/main/java/io/github/cleitonleite/localizacao/LocalizacaoApplication.java
+++ src/main/java/io/github/cleitonleite/localizacao/LocalizacaoApplication.java
@@ -1,5 +1,9 @@
 package io.github.cleitonleite.localizacao;
 
+import io.github.cleitonleite.localizacao.domain.entity.Cidade;
+import io.github.cleitonleite.localizacao.domain.repository.CidadeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +11,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Inicializado!");
+		salvarCidade();
+		listarCidade();
 	}
+
+	@Transactional
+	void salvarCidade() {
+		var cidade = new Cidade(1L, "Recife", 1488920L);
+		cidadeRepository.save(cidade);
+	}
+
+	void listarCidade() {
+		cidadeRepository.findAll().forEach(System.out::println);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(LocalizacaoApplication.class, args);
 	}
